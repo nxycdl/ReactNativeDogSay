938d4517ba2551cb64d24cc8a862b7d032dbd45a
nxycdl <nxycdl@163.com>
2017-04-11 16:13:34 +0800

    upload image

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 /**
  * Created by Administrator on 2017-04-10.
  */
-var list = [];
+var _ = require('lodash');
+var list = [{uri: '11112'},{uri: '11113'},{uri: '11114'}];
 var source = {uri: '1111'};
 list.push(source);
+console.log(list);
+var i = _.remove(list,source);
+console.log('x',i);
 console.log(list);
